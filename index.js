0379571c03720739500dbddf377bcf4d0dfee332
Enzo <120275012+Ko-rya@users.noreply.github.com>
2023-05-11 22:09:09 +0200

    Correct etCount showing up twice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const hacker1 = "Enzo";
+const hacker1 = "John";
 console.log(`The driver's name is ${hacker1}`);
-const hacker2 = "Tiago";
+const hacker2 = "Sarah";
 console.log(`The navigator's name is ${hacker2}`);
 
 if (hacker1.length > hacker2.length) {
@@ -22,23 +22,22 @@ if (comparison < 0) {
 } else {
   console.log("What?! You both have the same name?");
 }
-let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id feugiat nulla. Sed eu venenatis lectus. Suspendisse eros ligula, vehicula a auctor ut, aliquam eget felis. Fusce porttitor lorem quis nulla venenatis ultricies. Donec eleifend neque id venenatis euismod. Donec varius viverra fringilla. Suspendisse potenti. Cras mattis euismod ligula sit amet elementum. Suspendisse potenti. Nam risus felis, cursus ut faucibus at, tristique eget nisi. Integer pellentesque laoreet odio eu viverra. Curabitur euismod porttitor mauris vel euismod. Donec pellentesque volutpat est
-Fusce enim ante, ultrices nec facilisis sit amet, euismod vitae nibh. Donec interdum elit vitae tellus tristique, at interdum metus tempor. Sed eu placerat nisl, quis facilisis diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In egestas bibendum blandit. Etiam consequat volutpat feugiat. Curabitur rutrum enim dui, id imperdiet felis facilisis vel. Pellentesque ante dui, mattis id purus a, cursus feugiat nunc.
-Cras augue lacus, tincidunt sit amet porta et, feugiat a metus. Proin a neque vitae neque cursus auctor. Proin enim justo, dapibus eleifend suscipit eget, ultricies at nulla. Maecenas eu orci consequat, placerat sem sit amet, consequat lorem. Vestibulum suscipit non augue ut suscipit. Sed vitae vehicula mi. Curabitur sit amet egestas elit. Suspendisse gravida, nulla a tempus ullamcorper, quam ante condimentum massa, volutpat varius libero lectus nec lectus. Cras feugiat luctus dolor, elementum feugiat arcu semper ut. Pellentesque quis ullamcorper leo, in finibus libero. Nam ac est lobortis, tincidunt elit vel, tincidunt lectus. Nulla tempor ullamcorper augue, ut semper sapien imperdiet ultrices. Duis condimentum aliquam orci, nec tempor diam maximus nec."
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id feugiat nulla. Sed eu venenatis lectus. Suspendisse eros ligula, vehicula a auctor ut, aliquam eget felis. Fusce porttitor lorem quis nulla venenatis ultricies. Donec eleifend neque id venenatis euismod. Donec varius viverra fringilla. Suspendisse potenti. Cras mattis euismod ligula sit amet elementum. Suspendisse potenti. Nam risus felis, cursus ut faucibus at, tristique eget nisi. Integer pellentesque laoreet odio eu viverra. Curabitur euismod porttitor mauris vel euismod. Donec pellentesque volutpat est. Fusce enim ante, ultrices nec facilisis sit amet, euismod vitae nibh. Donec interdum elit vitae tellus tristique, at interdum metus tempor. Sed eu placerat nisl, quis facilisis diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In egestas bibendum blandit. Etiam consequat volutpat feugiat. Curabitur rutrum enim dui, id imperdiet felis facilisis vel. Pellentesque ante dui, mattis id purus a, cursus feugiat nunc. Cras augue lacus, tincidunt sit amet porta et, feugiat a metus. Proin a neque vitae neque cursus auctor. Proin enim justo, dapibus eleifend suscipit eget, ultricies at nulla. Maecenas eu orci consequat, placerat sem sit amet, consequat lorem. Vestibulum suscipit non augue ut suscipit. Sed vitae vehicula mi. Curabitur sit amet egestas elit. Suspendisse gravida, nulla a tempus ullamcorper, quam ante condimentum massa, volutpat varius libero lectus nec lectus. Cras feugiat luctus dolor, elementum feugiat arcu semper ut. Pellentesque quis ullamcorper leo, in finibus libero. Nam ac est lobortis, tincidunt elit vel, tincidunt lectus. Nulla tempor ullamcorper augue, ut semper sapien imperdiet ultrices. Duis condimentum aliquam orci, nec tempor diam maximus nec."
 console.log(longText.split(" ").length);
 let etCount = 0
-let wordsArray = longText.split(" ").length
-for (let i=0; i<wordsArray.length<i++){
-if (wordsArray[i]===et){
-    count++
+let wordsArray = longText.split(" ")
+let wordsArrayLength = wordsArray.length
+for (let i=0; i<wordsArray.length; i++){
+if (wordsArray[i]=== "et"|| wordsArray[i]=== "et,"||wordsArray[i]=== "et."|| wordsArray[i]=== ",et"){
+    etCount++
 }
 }
 console.log(etCount)
+
 let text = "A man, a plan, a canal, Panama!"
 let textChecker = text.toUpperCase().split().reverse().join()
 if (text=textChecker){
-    console.log("that's a palindrome")
+    console.log("that's a palindrome")}
 else {
     console.log("that is not a palindrom")
-}
 }
